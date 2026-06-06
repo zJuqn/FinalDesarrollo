b97ad56c126774da1a1152530ba85e92a2dd51d1
Juan Jose Coronel Perez <juanjocoronelp12@gmail.com>
2026-06-06 10:50:13 -0500

    GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/FinalDesarrollo/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
